afbe8ed4ea6575d00d2f618094a6364fcda47476
Alfonso Pruneda-Suarez <alfonso.pru@outlook.com>
2019-09-14 22:25:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,9 @@
+reversed_phrase = ""
+
 def reverse_each_word(phrase)
   new_phrase = phrase.split(" ")
+  new_phrase.each do |word|
+    reversed_phrase += word.reverse
+  end
+  reversed_phrase
 end
